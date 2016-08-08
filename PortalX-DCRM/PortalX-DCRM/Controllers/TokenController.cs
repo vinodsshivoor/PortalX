9f1d76c4e3b70cbd68af091087c2336fbc46e32c
Pramod Dhokane <Pramod Dhokane>
2016-08-08 18:14:02 +0530

    Oauth impliment

---
+++ PortalX-DCRM/PortalX-DCRM/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace PortalX_DCRM.Controllers
+{
+    public class TokenController : ApiController
+    {
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        [Authorize]
+        [Route("api/token/ChangePassword")]
+        [HttpPost] // There are HttpGet, HttpPost, HttpPut, HttpDelete.
+        public string ChangePassword()
+        {
+            return "Changesd";
+        }
+
+        [Route("api/token/getToken")]
+        [HttpPost] 
+        public string getToken()
+        {
+            return "genToken";
+        }
+    }
+}
--- PortalX-DCRM/PortalX-DCRM/Startup.cs
+++ PortalX-DCRM/PortalX-DCRM/Startup.cs
@@ -15,28 +15,28 @@ namespace PortalX_DCRM
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
-          //  ConfigureOAuth(app);
+            ConfigureOAuth(app);
 
           
             WebApiConfig.Register(config);
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
         }
-                
-        //public void ConfigureOAuth(IAppBuilder app)
-        //{
-        //    OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
-        //    {
-        //        AllowInsecureHttp = true,
-        //        TokenEndpointPath = new PathString("/token"),
-        //        AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
-        //        Provider = new CrmAuthorizationServerProvider()
-        //    };
 
-        //    // Token Generation
-        //    app.UseOAuthAuthorizationServer(OAuthServerOptions);
-        //    app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/AccessToken"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new CrmAuthorizationServerProvider()
+            };
 
-        //}
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
     }
 }
